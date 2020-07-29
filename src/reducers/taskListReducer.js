01129bf543aff0c26adde63c2281fb8b548738c1
Carlos3dg <carlosoo3dg@gmail.com>
2020-07-29 10:11:06 -0500

    Enable the rest of the repeat options, finish ADD_TASK case

--- src/reducers/taskListReducer.js
+++ src/reducers/taskListReducer.js
@@ -1,5 +1,6 @@
 import {v4 as uuidv4} from 'uuid';
 import {getIndexWeekAndDay} from '../helpers/calendarHelpers';
+import {getMonth} from '../helpers/calendarHelpers';
 
 export default function taskListReducer(
     state=[],
@@ -82,16 +83,84 @@ function getNewTask(newTask, oldState, action) {
             return newState;
         }
         case 'Monthly': {
+            if(taskDate.day === 31) {
+                for(let month=taskDate.month; month<=11; month++) {
+                    const fullMonth = getMonth(month, taskDate.year);
+                    const {week} = fullMonth[fullMonth.length - 1];
+                    let day;
+                    day = week.find((day, index) => {
+                        if(week[week.length - 1] === null) {
+                            return day > week[index+1];
+                        } else {
+                            return day === week[week.length - 1]
+                        }
+                    });
+                    taskDate.month = month;
+                    taskDate.day = day;
+                    newState = getYearTask(newTask, newState, taskDate);
+                }
+
+                return newState;
+            } else {
+                for(let month=taskDate.month; month<=11; month++) {
+                    taskDate.month = month;
+                    newState = getYearTask(newTask, newState, taskDate);
+                }
+
+                return newState
+            }
 
         }
         case 'Anually': {
-
+            for(let year = taskDate.year; year<=2040; year++) {
+                taskDate.year = year;
+                newState = getYearTask(newTask, newState, taskDate);
+            }
+            
+            return newState
         }
         case 'Weekends': {
+            const dayPosition = getIndexWeekAndDay(action.fullMonth, action.task.day);
+            let days = [];
+            action.fullMonth.forEach((element, index) => {
+                if(index >= dayPosition.week) {
+                    if(taskDate.day === element.week[element.week.length - 1]) {
+                        days = [...days, element.week[element.week.length - 1]];
+                    } else {
+                        days = [...days, element.week[0]];
+                        if(element.week[element.week.length - 1] !== null) days = [...days, element.week[element.week.length - 1]]
+                    }
+                }
+            });
 
+            days.forEach((day) => {
+                taskDate.day = day;
+                newState = getYearTask(newTask, newState, taskDate);
+            });
+
+            return newState;
         }
         case 'Every weekday (Monday to Friday)': {
+            const dayPosition = getIndexWeekAndDay(action.fullMonth, action.task.day);
+            let days = [];
+            action.fullMonth.forEach(function(element, index) {
+                if(index >= dayPosition.week) {
+                    const newDays = element.week.filter((day, index) => {
+                        if(index !== 0 && index!==6 && day !== null && day >= taskDate.day) {
+                            return day;
+                        }
+                    });
 
+                    days = [...days, ...newDays];
+                }
+            });
+
+            days.forEach((day) => {
+                taskDate.day = day;
+                newState = getYearTask(newTask, newState, taskDate);
+            });
+
+            return newState;
         }
     }
 }
